b57914a89079f83874511ba9e482ffed6dabecae
meltem <meltemkaraagac19@gmail.com>
2020-12-26 13:09:54 -0500

    add

--- src/fscohort/urls.py
+++ src/fscohort/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home_view, about
+from .views import home_view, student_list, student_add
 
 urlpatterns = [
     path("", home_view),
-    path("about/", about)
+    path("list/", student_list),
+    path("add/", student_add),
+    # path("about/", about)
 ]
--- src/fscohort/views.py
+++ src/fscohort/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import StudentForm
+from .models import Student
 
 
 def home_view(request):
@@ -9,18 +10,36 @@ def home_view(request):
     # print(request.user)
     # print(request.path)
     # print(request.method)
-    form = StudentForm()
-    my_context = {
-        'title': '<b>clarusway</b>',
-        'dict_1': {'djang': 'best framework'},
-        'my_list': [2, 3, 4, 5],
-        'cat': 'maviş',
-        'student': form
-    }
-    return render(request, "fscohort/home.html", my_context)
+    # form = StudentForm()
+    # my_context = {
+    #     'title': '<b>clarusway</b>',
+    #     'dict_1': {'djang': 'best framework'},
+    #     'my_list': [2, 3, 4, 5],
+    #     'cat': 'maviş',
+    #     'student': form
+    # }
+    return render(request, "fscohort/home.html")
 
 # Create your views here.
 
 
-def about(request):
-    return HttpResponse("About page.")
+def student_list(request):
+    students = Student.objects.all()
+    context = {
+        'students': students
+    }
+    return render(request, "fscohort/student_list.html", context)
+
+
+def student_add(request):
+    form = StudentForm()
+    if request.method == "POST":
+        print(request.POST)
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {
+        'form': form
+    }
+    return render(request, "fscohort/student_add.html", context)
