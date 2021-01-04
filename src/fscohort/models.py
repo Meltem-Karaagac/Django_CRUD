93cc228afbec131e7b153d6d1741a948b5825944
meltem <meltemkaraagac19@gmail.com>
2021-01-03 22:15:43 -0500

    add

--- src/fscohort/models.py
+++ src/fscohort/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Student(models.Model):
-    first_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=50)
     number = models.IntegerField()
 
