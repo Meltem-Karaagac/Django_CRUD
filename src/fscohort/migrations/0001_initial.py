4d12dc2352789bdd298f028ee06033005dd95f13
meltem <meltemkaraagac19@gmail.com>
2021-01-04 23:54:27 -0500

    added

--- src/fscohort/migrations/0001_initial.py
+++ src/fscohort/migrations/0001_initial.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True,
                                         primary_key=True, serialize=False, verbose_name='ID')),
-                ('first_name', models.CharField(max_length=50)),
+                ('first_name', models.CharField(max_length=70)),
                 ('last_name', models.CharField(max_length=50)),
                 ('number', models.IntegerField()),
             ],
